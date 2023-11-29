2bf1716da36ae518f57e59450d5093bf57fcb7ee
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-11-29 13:00:19 -0300

    Update OnceIdeal

--- Frontend/src/views/OnceIdeal/OnceIdeal.jsx
+++ Frontend/src/views/OnceIdeal/OnceIdeal.jsx
@@ -1,5 +1,10 @@
-export function OnceIdeal(){
-    return(
-        <h1>Once Ideal Works...</h1>
+import "./OnceIdeal.css";
+
+export function OnceIdeal() {
+    return (
+        <main className="onceIdealMain">
+            <h1 className="onceIdealMain__titulo">¡Arma tu selección!</h1>
+            <p className="onceIdealMain__descripcion">Crea tu once ideal con los jugadores más destacados de la copa. Selecciona estratégicamente tus jugadores y compartí  tu equipo con amigos. ¡Demostra tu conocimiento!</p>
+        </main>
     );
 }
