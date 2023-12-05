411f47524189baedf76d953444faea78ebf3b6b6
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-12-05 12:48:38 -0300

    Update NotFound

--- Frontend/src/views/404/NotFound.jsx
+++ Frontend/src/views/404/NotFound.jsx
@@ -1,8 +1,11 @@
+import "./NotFound.css";
+
 export function NotFound() {
     return (
-        <>
-            <h1>ERROR 404</h1>
-            <p>La direccion web no existe en el sitio.</p>
-        </>
+        <main className="notFoundMain">
+            <h1 className="notFound__titulo">ERROR 404</h1>
+            <p>La direccion web no existe en el sitio, Chile e Italia no jugaron en Qatar 2022.</p>
+
+        </main>
     );
 }
